668b3f91b92474966e7c534d6ea4d50e2878b941
Suyog Luitel <suyogluitel235@gmail.com>
2024-06-14 13:48:59 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Header from './containers/header/Header'
+import Brand from './components/brand/Brand'
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3'
+import Features from './containers/features/Features'
+import Possibility from './containers/possibility/Possibility'
+import CTA from './components/cta/CTA'
+import Blog from './containers/blog/Blog'
+import Footer from './containers/footer/Footer'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
